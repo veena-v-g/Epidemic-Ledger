0c9e0a52a25870963b510164e26467b289c0b5b7
Tristan Slater <hello@trslater.ca>
2018-10-13 16:31:17 -0700

    Finish registration/login system

--- server/views.py
+++ server/views.py
@@ -1,21 +1,71 @@
-from flask import render_template, flash, redirect, \
-    url_for
+from flask import render_template, flash, request, redirect, url_for
 from server import app
-from server.forms import LoginForm
+from server import db
+from server.models import User
+from server.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 
 @app.route("/")
 def index():
-    return "Home page"
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash("You've registered")
+
+        return redirect(url_for("login"))
+
+    return render_template("register.html", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        flash(f"Login requested for user {form.username.data}")
+        user = User.query.filter_by(username=form.username.data).first()
 
-        return redirect(url_for("index"))
+        if user is None or not user.check_password(form.password.data):
+            flash("No!")
+            return redirect(url_for("login"))
+
+        login_user(user)
+
+        next_page = request.args.get("next")
+
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+
+        return redirect(next_page)
 
     return render_template("login.html", form=form)
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/private")
+@login_required
+def private():
+    return render_template("private.html")
