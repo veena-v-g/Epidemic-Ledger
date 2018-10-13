06d96314d2476ef759a30caadb4aadc4f8ecda0a
FrankWhoee <huifrank@live.com>
2018-10-13 13:16:56 -0700

    Add five number summary

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -1,35 +1,36 @@
 import numpy as np
 import csv
+from numpy import percentile
+from numpy import array
 
 
-def get_five_num_summ(data):
+def get_five_num_summ(numbers):
 
-    numpy_array = np.array(data)
+    for index in range(len(numbers)):
+        numbers[index] = int(numbers[index].replace("\"", ""))
+        print(type(numbers[index]))
 
-    list.sort(data)
+    list.sort(numbers)
 
     # Get minimum
-    m = data[0]
-
-    # Get first quartile
-    Q1 = np.percentile(data, 25)
-
-    # Get median
-    Q2 = np.percentile(data, 50)
-
-    # Get third quartile
-    Q3 = np.percentile(data, 75)
+    m = numbers[0]
 
     # Get maximum
-    M = np.percentile(data, 25)
+    M = numbers[len(numbers) - 1]
 
-    return [m, Q1, Q2, Q3, M]
+    numbers = np.asarray(numbers)
+    # Get 1st, 3rd quartiles and median
+    quartiles = percentile(numbers, [25, 50, 75])
+    five_num_summary = [m]
+    five_num_summary += list(quartiles)
+    five_num_summary += [M]
+
+    return five_num_summary
 
 
 def get_statistics_country(country,index):
     with open('../Datasets/Mosquito/Zika/cdc_zika.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-
         summary = {}
         for row in reader:
             list = row[0].split(",")
@@ -43,7 +44,30 @@ def get_statistics_country(country,index):
 
         return summary
 
-summary = get_statistics_country("Argentina",3)
-print("Statistics for Argentina")
-for key in summary:
-    print(key + ": " + str(summary[key]))
+
+def get_world_summary():
+    countries = []
+    world_summary = {}
+    with open('../Datasets/Mosquito/Zika/cdc_zika.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in reader:
+            list = row[0].split(",")
+            if len(list) > 1:
+                country = list[1][1:list[1].find("-")]
+                if country not in countries and country != "location":
+                    countries.append(country)
+
+        for country in countries:
+            # print(country)
+            country_summary = get_statistics_country(country, 3)
+            for case in country_summary:
+                if "confirmed" in case or "probable" in case:
+                    if country in world_summary:
+                        world_summary[country] += country_summary[case]
+                    else:
+                        world_summary[country] = country_summary[case]
+        return country_summary;
+
+
+summary = get_world_summary()
+print(str(get_five_num_summ(list(summary.values()))))
