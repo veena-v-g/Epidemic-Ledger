8a7239addea82b2da9e581430b7762d68ff457e5
Tristan Slater <hello@trslater.ca>
2018-10-14 11:21:15 -0700

    Downgrade usage to python 3.4 syntax

--- server/models.py
+++ server/models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return "<User {}>".format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
