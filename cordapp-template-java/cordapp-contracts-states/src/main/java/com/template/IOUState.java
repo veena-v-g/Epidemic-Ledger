b523f4d80c42abc9d0a03f4a117e26cc2f862445
Veena Ghorakavi (Emma) <vghorakavi@gmail.com>
2018-10-13 20:24:58 -0700

    Add files via upload

---
+++ cordapp-template-java/cordapp-contracts-states/src/main/java/com/template/IOUState.java
@@ -0,0 +1,40 @@
+package com.template;
+
+import com.google.common.collect.ImmutableList;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
+
+import java.util.List;
+
+/**
+ * Define your state object here.
+ */
+public class IOUState implements ContractState {
+    private final int value;
+    private final Party lender;
+    private final Party borrower;
+
+    public IOUState(int value, Party lender, Party borrower) {
+        this.value = value;
+        this.lender = lender;
+        this.borrower = borrower;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Party getLender() {
+        return lender;
+    }
+
+    public Party getBorrower() {
+        return borrower;
+    }
+
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return ImmutableList.of(lender, borrower);
+    }
+}
