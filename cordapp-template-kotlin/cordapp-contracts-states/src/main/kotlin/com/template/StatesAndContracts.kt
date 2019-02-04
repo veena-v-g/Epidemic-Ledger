d9ea122764fc811f95fe88d83a3a80f6a5df3314
Veena Ghorakavi (Emma) <vghorakavi@gmail.com>
2019-02-04 02:16:02 -0600

    Added PatientState and DiagnosisContract.
    Diagnosis is contract for state which changes diagnosis and confirmed members of PatientState.  Uploading files from terminal is not working so I am doing file uploads manually.

--- cordapp-template-kotlin/cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
+++ cordapp-template-kotlin/cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -4,32 +4,40 @@ import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
 import net.corda.core.contracts.ContractState
 import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
 import net.corda.core.transactions.LedgerTransaction
 
 // ************
 // * Contract *
 // ************
-class TemplateContract : Contract {
+class DiagnosisContract : Contract {
     companion object {
         // Used to identify our contract when building a transaction.
-        const val ID = "com.template.TemplateContract"
-    }
-    
-    // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
-    // does not throw an exception.
-    override fun verify(tx: LedgerTransaction) {
-        // Verification logic goes here.
+        const val ID = "com.template.DiagnosisContract"
     }
 
     // Used to indicate the transaction's intent.
     interface Commands : CommandData {
         class Action : Commands
+        class Diagnosis: Commands
+    }
+
+    // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
+    // does not throw an exception.
+    override fun verify(tx: LedgerTransaction) {
+        // Verification logic goes here.
     }
 }
 
 // *********
 // * State *
 // *********
-data class TemplateState(val data: String) : ContractState {
-    override val participants: List<AbstractParty> = listOf()
+data class PatientState(val patientName: String,
+                        val doctorName: Party,
+                        val hospitalName: Party,
+                        val diagnosis: String,
+                        val treatment: String,
+                        val confirmed: Char,
+                        val data: String) : ContractState {
+    override val participants: List<Party>get() = listOf(doctorName, hospitalName)
 }
