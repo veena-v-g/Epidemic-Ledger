d0c0fe8ba5bd61ca9232ef7550c37b23e9c8a6a2
Veena Ghorakavi (Emma) <vghorakavi@gmail.com>
2019-02-05 01:44:33 -0600

    Atomicity for contracts.
    Contracts or diagnoses will be made once transaction is complete for each state.  State is a node and contract is pointer/descriptor to node.  Also, I finally found the jar file.  No more manual uploads!

--- cordapp-template-kotlin/cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
+++ cordapp-template-kotlin/cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -6,6 +6,14 @@ import net.corda.core.contracts.ContractState
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
 import net.corda.core.transactions.LedgerTransaction
+import net.corda.core.contracts.*
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import net.corda.core.schemas.QueryableState
+import net.corda.core.serialization.CordaSerializable
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
 
 // ************
 // * Contract *
@@ -13,19 +21,25 @@ import net.corda.core.transactions.LedgerTransaction
 class DiagnosisContract : Contract {
     companion object {
         // Used to identify our contract when building a transaction.
-        const val ID = "com.template.DiagnosisContract"
+        const val DIAGNOSIS_CONTRACT_ID = "com.template.DiagnosisContract"
     }
 
     // Used to indicate the transaction's intent.
     interface Commands : CommandData {
-        class Action : Commands
         class Diagnosis: Commands
+        class Verification: Commands
+        class Treatment: Commands
+        class Progress: Commands
     }
 
     // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
     // does not throw an exception.
     override fun verify(tx: LedgerTransaction) {
         // Verification logic goes here.
+        val command = tx.commands.requireSingleCommand<Commands>()
+        requireThat{
+        "No diagnosis should be consumed when sending a message".using (tx.inputs.isEmpty())
+        }
     }
 }
 
@@ -35,9 +49,7 @@ class DiagnosisContract : Contract {
 data class PatientState(val patientName: String,
                         val doctorName: Party,
                         val hospitalName: Party,
-                        val diagnosis: String,
-                        val treatment: String,
-                        val confirmed: Char,
+                        val insuranceName: Party,
                         val data: String) : ContractState {
     override val participants: List<Party>get() = listOf(doctorName, hospitalName)
 }
